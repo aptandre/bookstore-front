3078a32b1986cbdbfc88f532f54d9b3ab157a5d1
Andre <andre.alves@dilettasolutions.com>
2021-12-22 14:07:13 -0300

    Adicionando o botão

--- src/app/components/views/categoria/categoria/categoria-read/categoria-read.component.ts
+++ src/app/components/views/categoria/categoria/categoria-read/categoria-read.component.ts
@@ -9,9 +9,22 @@ import { CategoriaService } from '../categoria.service';
 })
 export class CategoriaReadComponent implements OnInit {
 
+  ELEMENT_DATA: Categoria[] = [
+    {id: '1', nome: 'Hydrogen', descricao: "Lorem Ipsum"},
+    {id: '2', nome: 'Helium', descricao: "Lorem Ipsum"},
+    {id: '3', nome: 'Lithium', descricao: "Lorem Ipsum"},
+    {id: '4', nome: 'Beryllium', descricao: "Lorem Ipsum"},
+    {id: '5', nome: 'Boron', descricao:"Lorem Ipsum"},
+    {id: '6', nome: 'Carbon', descricao: "Lorem Ipsum"},
+    {id: '7', nome: 'Nitrogen', descricao: "Lorem Ipsum"},
+    {id: '8', nome: 'Oxygen', descricao: "Lorem Ipsum"},
+    {id: '9', nome: 'Fluorine', descricao: "Lorem Ipsum"},
+    {id: '10', nome: 'Neon', descricao: "Lorem Ipsum"},
+  ];
   categorias: Categoria[] = []
 
   displayedColumns: string[] = ['id', 'nome', 'descricao','livros', 'acoes'];
+  dataSource = this.ELEMENT_DATA;
 
   constructor(private service: CategoriaService) {  }
 
